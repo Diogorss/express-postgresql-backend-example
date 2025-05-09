0813dd1397c00d553c873fdd298f522b7058f250
DIogo Rossi Sampaio <108912348+Diogorss@users.noreply.github.com>
2025-05-09 00:54:50 +0000

    corrigindo

--- example_requests/POST_Register_User.sh
+++ example_requests/POST_Register_User.sh
@@ -1,4 +1,4 @@
-curl -X POST http://localhost:3000/user/register \
+curl --request POST \
   --url 'https://express-postgresql-backend-example-khaki.vercel.app/user/register' \
   -H "Content-Type: application/json" \
   -d '{ "username": "newuser","password": "securepassword123" }'
