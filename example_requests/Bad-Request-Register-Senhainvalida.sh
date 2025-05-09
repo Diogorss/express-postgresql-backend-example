39eef1e08ab813475bb4980859b7583e1ca4912c
DIogo Rossi Sampaio <108912348+Diogorss@users.noreply.github.com>
2025-05-09 12:57:30 +0000

    finalizando ajustes

--- example_requests/Bad-Request-Register-Senhainvalida.sh
+++
@@ -1,4 +0,0 @@
-curl --request POST \
-  --url https://express-postgresql-backend-example-khaki.vercel.app/user/register \
-  -H "Content-Type: application/json" \
-  -d '{"username": "user02", "password": "123"}'
--- example_requests/POST_Login_User.sh
+++ example_requests/POST_Login_User.sh
@@ -2,6 +2,6 @@ curl --request POST \
   --url 'https://express-postgresql-backend-example-khaki.vercel.app/user/login' \
   --header 'Content-Type: application/json' \
   --data '{
-    "username": "newuser",
+    "username": "newuser3",
     "password": "securepassword123"
     }'
